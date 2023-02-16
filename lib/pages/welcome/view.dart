aa0f4585e03a55c7a64b7c02175fc278bc43b1cd
yavuzmutlu <selim.mutlu@bil.omu.edu.tr>
2023-02-16 16:26:44 +0300

    update

--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/pages/welcome/controller.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class WelcomePage extends GetView<WelcomeController> {
@@ -8,10 +9,23 @@ class WelcomePage extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-        child: Text("Proje welcome sayfası"),
-      )),
-    );
+        body: Obx(() => SizedBox(
+              width: 360.w,
+              height: 780.w,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  PageView(
+                    scrollDirection: Axis.horizontal,
+                    reverse: false,
+                    onPageChanged: (index) {},
+                    controller: PageController(
+                        initialPage: 0, keepPage: false, viewportFraction: 1),
+                    pageSnapping: true,
+                    physics: const ClampingScrollPhysics(),
+                  )
+                ],
+              ),
+            )));
   }
 }
