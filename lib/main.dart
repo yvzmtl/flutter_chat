0e01ba4732c6d0f58190ce4493f078126760dba7
yavuzmutlu <selim.mutlu@bil.omu.edu.tr>
2023-03-01 16:49:50 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/firebase_options.dart';
 import 'package:flutter_chat/routes/pages.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 Future<void> main() async {
@@ -16,19 +17,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      builder: (BuildContext context, Widget? child) => GetMaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+        // home: Scaffold(
+        //   body: Center(
+        //       child: Container(
+        //     child: Text("Proje başlangıcı"),
+        //   )),
+        // )
       ),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-      // home: Scaffold(
-      //   body: Center(
-      //       child: Container(
-      //     child: Text("Proje başlangıcı"),
-      //   )),
-      // )
     );
   }
 }
--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -9,23 +9,56 @@ class WelcomePage extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Obx(() => SizedBox(
-              width: 360.w,
-              height: 780.w,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  PageView(
-                    scrollDirection: Axis.horizontal,
-                    reverse: false,
-                    onPageChanged: (index) {},
-                    controller: PageController(
-                        initialPage: 0, keepPage: false, viewportFraction: 1),
-                    pageSnapping: true,
-                    physics: const ClampingScrollPhysics(),
-                  )
-                ],
-              ),
-            )));
+      body: SizedBox(
+        width: 360.w,
+        height: 780.w,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            PageView(
+              scrollDirection: Axis.horizontal,
+              reverse: false,
+              onPageChanged: (index) {},
+              controller: PageController(
+                  initialPage: 0, keepPage: false, viewportFraction: 1),
+              pageSnapping: true,
+              physics: const ClampingScrollPhysics(),
+              children: [
+                // 1. banner
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage("assets/images/banner1.png"),
+                  )),
+                ),
+                // 2. banner
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage("assets/images/banner1.png"),
+                  )),
+                ),
+                //3. banner
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage("assets/images/banner1.png"),
+                  )),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
